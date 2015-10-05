82ef70f1258c9d3ef05437664b408b0825394492
alexvictoor <alexvictoor@gmail.com>
2015-10-05 22:17:44 +0200

    Fix demo

--- BrowserLog.Demo/Program.cs
+++ BrowserLog.Demo/Program.cs
@@ -17,20 +17,34 @@ namespace BrowserLog.Demo
             XmlConfigurator.Configure(new FileInfo("log4net.xml"));
 
             var logger = LogManager.GetLogger(typeof(Program));
-            logger.Info("Hello this is a log from a server-side process!");
-            logger.Warn("Hello this is a log from a server-side process!");
-            logger.Error("Hello this is a log from a server-side process!", new Exception());
             Thread.Sleep(10000);
-            logger.Error("Hello this is a log from a server-side process!", new Exception());
             for (int i = 0; i < 100000; i++)
             {
-                Thread.Sleep(100);
-                logger.DebugFormat("... and here is another one again ({0})", i);
-                logger.Info("An error??", new Exception());
-                Thread.Sleep(10000);
-                logger.Warn("Hello this is a log from a server-side process!");
-                logger.Error("Hello this is a log from a server-side process!");
+                logger.Info("Hello this is a log from a server-side process!");
+                Thread.Sleep(1000);
+                logger.Warn("Hello this is a warning from a server-side process!");
+                logger.DebugFormat("... and here is another log again ({0})", i);
+                Thread.Sleep(2000);
+                try
+                {
+                    ThrowExceptionWithStackTrace(4);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("An error has occured, really?", ex);    
+                }
+                
+                Thread.Sleep(1000);            
             }
+        }
+
+        static void ThrowExceptionWithStackTrace(int depth)
+        {
+            if (depth == 0)
+            {
+                throw new Exception("A fake exception to show an example");
+            }
+            ThrowExceptionWithStackTrace(--depth);
         }
     }
 }
