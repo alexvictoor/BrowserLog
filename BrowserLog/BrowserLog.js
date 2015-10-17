08bbcb9950562614e73576c80aa2f529398fb47a
alexvictoor <alexvictoor@gmail.com>
2015-10-17 15:36:26 +0200

    raise an alert on old browers

--- BrowserLog/BrowserLog.js
+++ BrowserLog/BrowserLog.js
@@ -7,19 +7,21 @@
         var streamUrl = scriptUrl.substring(0, scriptUrl.length - 13) + "stream"; // 13 being the length of string "BrowserLog.js"
         var source = new EventSource(streamUrl);
         source.onmessage = function(event) {
-           console.log(event.data);
+            console.log(event.data);
         };
-        source.addEventListener("DEBUG", function (event) { 
+        source.addEventListener("DEBUG", function(event) {
             console.debug(event.data);
         });
-        source.addEventListener("INFO", function (event) { 
+        source.addEventListener("INFO", function(event) {
             console.info(event.data);
         });
-        source.addEventListener("WARN", function (event) { 
+        source.addEventListener("WARN", function(event) {
             console.warn(event.data);
         });
-        source.addEventListener("ERROR", function (event) { 
+        source.addEventListener("ERROR", function(event) {
             console.error(event.data);
         });
+    } else {
+        alert("Your browser does not support SSE, hence BrowserLog will not work properly");
     }
 })();
