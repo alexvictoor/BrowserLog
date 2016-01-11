da981eb938fb5612022573c1d6456b3e09f95b4d
momo3038 <morgan.leroi@gmail.com>
2016-01-11 23:56:30 +0100

    First draft in order to add NLog Support to BrowserLog

---
+++ BrowserLog.NLog.Tests/BrowserConsoleTargetTest.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using BrowserLog.TinyServer;
+using NLog;
+using NLog.Layouts;
+using NLog.Targets;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+
+namespace BrowserLog
+{
+    public class BrowserConsoleTargetTest
+    {
+        private IEventChannel _channel;
+        private BrowserConsoleTarget _target;
+
+        [SetUp]
+        public void ConfigureLogger()
+        {
+            //var layout = new SimpleLayout("%-4timestamp [%thread] %-5level %logger %ndc - %message%newline");
+            var layout = new SimpleLayout("${date:format=yyyyMMddHHmmss} ${message}");
+            
+
+            var channelFactory = Substitute.For<ChannelFactory>();
+            _channel = Substitute.For<IEventChannel>();
+            channelFactory.Create("localhost", 8765, 1).Returns(_channel);
+            _target = new BrowserConsoleTarget(channelFactory)
+            {
+                Host = "localhost",
+                Port = 8765,
+                Layout = layout,
+                Buffer = 1,
+                Name = "ConsoleTest"
+            };
+
+            
+            //_target.InitializeTarget();
+           
+            //layout.ActivateOptions();
+            
+        }
+
+        [Test]
+        public void Should_have_no_side_effect_if_active_flag_set_to_false()
+        {
+            // given
+          LogManager.Configuration.AddTarget(_target);
+           // BasicConfigurator.Configure(_target);
+            // when
+            LogManager.GetLogger(_target.Name, GetType()).Info("Everything's fine with NLog");
+            // then
+            _channel.DidNotReceive().Send(Arg.Any<ServerSentEvent>(), Arg.Any<CancellationToken>());
+        }
+
+        [Test]
+        public void Should_send_an_sse_message_when_receiving_a_logging_event()
+        {
+            // given
+            _target.Active = true;
+            
+            LogManager.Configuration.AddTarget(_target);
+            // when
+            LogManager.GetLogger(_target.Name, GetType()).Info("Everything's fine with NLog");
+            // then
+            _channel.Received().Send(Arg.Is<ServerSentEvent>(evt => evt.ToString().Contains("Everything's fine with NLog")), Arg.Any<CancellationToken>());
+        }
+
+        //[Test]
+        //public void Should_send_an_sse_message_with_a_type_matching_received_logging_event_level()
+        //{
+        //    // given
+        //    _target.Active = true;
+        //    _target.ActivateOptions();
+        //    BasicConfigurator.Configure(_target);
+        //    // when
+        //    LogManager.GetLogger(GetType()).Warn("level?");
+        //    // then
+        //    _channel.Received().Send(Arg.Is<ServerSentEvent>(evt => evt.ToString().StartsWith("event: WARN")), Arg.Any<CancellationToken>());
+        //}
+
+        //[Test]
+        //public void Should_send_an_sse_message_without_type_when_received_logging_event_level_has_no_matching_level_on_browser()
+        //{
+        //    // given
+        //    _target.Active = true;
+        //    _target.ActivateOptions();
+        //    BasicConfigurator.Configure(_target);
+        //    // when
+        //    LogManager.GetLogger(GetType()).Fatal("No fatal logs on the browser");
+        //    // then
+        //    _channel.Received().Send(Arg.Is<ServerSentEvent>(evt => evt.ToString().StartsWith("data:")), Arg.Any<CancellationToken>());
+        //}
+
+        //[Test]
+        //public void Should_send_a_multiline_sse_message_received_logging_event_for_an_exception()
+        //{
+        //    // given
+        //    _target.Active = true;
+        //    _target.ActivateOptions();
+        //    BasicConfigurator.Configure(_target);
+        //    // when
+        //    LogManager.GetLogger(GetType()).Warn("An error has occured", new Exception());
+        //    // then
+        //    var lineSeparator = new string[] {"\r\n"};
+        //    _channel.Received().Send(
+        //        Arg.Is<ServerSentEvent>(
+        //            evt => evt.ToString()
+        //                .Split(lineSeparator, StringSplitOptions.RemoveEmptyEntries)
+        //                .Skip(1)
+        //                .All(l => l.StartsWith("data:"))
+        //            ), Arg.Any<CancellationToken>());
+        //}
+
+        //[Test]
+        //public void Should_dispose_channel_on_shutdown()
+        //{
+        //    // given
+        //    _target.Active = true;
+        //    _target.ActivateOptions();
+        //    BasicConfigurator.Configure(_target);
+        //    // when
+        //    LogManager.Shutdown();
+        //    // then
+        //    _channel.Received().Dispose();
+        //}
+    }
+}
---
+++ BrowserLog.NLog/BrowserConsoleTarget.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using BrowserLog.TinyServer;
+using NLog;
+using NLog.Targets;
+
+namespace BrowserLog
+{
+    [Target("BrowserConsole")]
+    public class BrowserConsoleTarget : TargetWithLayout
+    {
+        private readonly ChannelFactory _channelFactory;
+        private IEventChannel _channel;
+
+        public bool Active { get; set; }
+        public int Port { get; set; }
+        public string Host { get; set; }
+        public int Buffer { get; set; }
+
+        public BrowserConsoleTarget()
+        {
+            _channelFactory = new ChannelFactory();
+            Active = true;
+            Port = 8765;
+            Buffer = 1;
+            Name = "Console";
+        }
+
+        
+        // for testing
+        public BrowserConsoleTarget(ChannelFactory channelFactory)
+        {
+            _channelFactory = channelFactory;
+        }
+
+//        protected override void InitializeTarget()
+//        {
+//            base.InitializeTarget();
+//
+//            if (Active)
+//            {
+//                if (Host == null)
+//                {
+//                    Host = FindLocalIp();
+//                }
+//                _channel = _channelFactory.Create(Host, Port, Buffer);
+//            }
+//        }
+
+        // hack described here
+        // http://stackoverflow.com/a/27376368
+        //
+        private static string FindLocalIp()
+        {
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("10.0.2.4", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var message = base.Layout.Render(logEvent);
+            var sse = new ServerSentEvent(logEvent.Level.Name, message);
+            _channel.Send(sse, new CancellationTokenSource(TimeSpan.FromSeconds(5)).Token);
+        }
+
+        protected override void CloseTarget()
+        {
+            if (_channel != null)
+            {
+                _channel.Dispose();
+            }
+            base.CloseTarget();
+        }
+    }
+}
