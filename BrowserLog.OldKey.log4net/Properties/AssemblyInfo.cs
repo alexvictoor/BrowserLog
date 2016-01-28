7fbff97432a937e07af94a8709e372329f522b49
alexvictoor <alexvictoor@gmail.com>
2016-01-28 21:15:57 +0100

    feat: integration with log4net-oldkey

---
+++ BrowserLog.OldKey.log4net/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BrowserLog.OldKey.log4net")]
+[assembly: AssemblyDescription("log4net appender based on HTML5 SSE - old key log4net version")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("BrowserLog.OldKey.log4net")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51f5750d-d1ff-49fa-8142-75fed0cd3c18")]
